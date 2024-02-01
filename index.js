fac4d58ded77b063dd1ffb9ef0c9ebdd1f851595
SNAaran <128003097+SNAaran@users.noreply.github.com>
2024-02-01 06:46:06 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI","Playing Minecraft","Scanning Server","On a call","Wathing Aran TechSpot"];
 
 
 let currentIndex = 0;
